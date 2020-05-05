b1ae88a8a2ed94ff5eb62eed0a1fe51e277f9603
Su-Kyung <slloph720@naver.com>
2020-05-05 16:28:11 +0900

    [CREATE] puzzle canvas, puzzle script

---
+++ RemedyFriends/Assets/Scripts/game2/game2_Put the Puzzle Together/game2_PuzzleGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class game2_PuzzleGenerator : MonoBehaviour
+{
+    
+    // 스크립트를 적용한 퍼즐조각
+    public Button piece;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // GameCountdown.cs의 enablespawn 가져오기
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();
+
+
+        piece.gameObject.SetActive(false);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GameCountdown.cs의 enablespawn 가져오기
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();
+
+        // 나중에 조건 하나 더 추가
+        if (Countdown.enableSpawn)
+        {
+            piece.gameObject.SetActive(true);
+        }
+    }
+}
