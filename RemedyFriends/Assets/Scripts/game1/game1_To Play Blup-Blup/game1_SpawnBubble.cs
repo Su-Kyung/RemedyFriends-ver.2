ae759730ada313735017d21b6a083fa6d16af470
Su-Kyung <slloph720@naver.com>
2020-05-18 23:17:23 +0900

    update bubble game (진행중)
    - 랜덤 숫자 부여 -> 중복 체크 안됨(이에 대해 진행중)
    - 디벨롭 이미지 적용
    - bubble UI변경 (이미지->버튼)
    
    + update .gitignore

--- RemedyFriends/Assets/Scripts/game1/game1_To Play Blup-Blup/game1_SpawnBubble.cs
+++ RemedyFriends/Assets/Scripts/game1/game1_To Play Blup-Blup/game1_SpawnBubble.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 // 게임 진행 + 점수 계산 한번에
@@ -11,30 +12,89 @@ public class game1_SpawnBubble : MonoBehaviour
     // 진주조개 방법으로 도전
     // 그러나 진주조개는 Clone으로 복제해서 랜덤생성하고 이 경우는 각각의 객체에
     // 랜덤 시간값을 줘서 각각이 랜덤생성 되도록 한다.(그래서 랜덤생성 및 삭제함수에서 각각 위치 지정함)
+    
+    public Button Bubble1, Bubble2, Bubble3, Bubble4, Bubble5;
 
-    public bool enableSpawn = false;
-    public GameObject Bubble1;
-    public GameObject Bubble2;
-    public GameObject Bubble3;
-    public GameObject Bubble4;
-    public GameObject Bubble5;
-
+    // 랜덤으로 순서 맞출 배열
+    // gameobject list 참고링크: https://m.blog.naver.com/PostView.nhn?blogId=hst322&logNo=220960510868&proxyReferer=https:%2F%2Fwww.google.co.kr%2F
+    List<Button> BubbleList = new List<Button>();
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBubble1", 0, 2.2f);
-        InvokeRepeating("SpawnBubble2", 0, 2.1f);
-        InvokeRepeating("SpawnBubble3", 0, 2.4f);
-        InvokeRepeating("SpawnBubble4", 0, 2.3f);
-        InvokeRepeating("SpawnBubble5", 0, 2.35f);
+        // 리스트에 버블 오브젝트들 추가
+        BubbleList.Add(Bubble1);
+        BubbleList.Add(Bubble2);
+        BubbleList.Add(Bubble3);
+        BubbleList.Add(Bubble4);
+        BubbleList.Add(Bubble5);
+
+        // 게임 시작 시 안보이게
+        Bubble1.gameObject.SetActive(false);
+        Bubble2.gameObject.SetActive(false);
+        Bubble3.gameObject.SetActive(false);
+        Bubble4.gameObject.SetActive(false);
+        Bubble5.gameObject.SetActive(false);
+
+        Shuffle(BubbleList);
+        //InvokeRepeating("SpawnBubble1", 0, 2.2f);
+        //InvokeRepeating("SpawnBubble2", 0, 2.1f);
+        //InvokeRepeating("SpawnBubble3", 0, 2.4f);
+        //InvokeRepeating("SpawnBubble4", 0, 2.3f);
+        //InvokeRepeating("SpawnBubble5", 0, 2.35f);
     }
+/*
+    void update()
+    {
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+
+        if (Countdown.enableSpawn)   // +발생해야 할 때에
+        {
+            for (int i = 0; i < 5; )
+            {
+
+            }
+        }
+    }
+*/
+    //for shuffle number from array
+    void Shuffle(List<Button> list)
+    {
+        int random1;
+        int random2;
+
+        Button tmp;
+
+        for (int index = 0; index < 5; ++index)
+        {
+            random1 = UnityEngine.Random.Range(0, 5);
+            random2 = UnityEngine.Random.Range(0, 5);
+
+            tmp = list[random1];
+            list[random1] = list[random2];
+            list[random2] = tmp;
+            Debug.Log(tmp + "는 " + random2 + "번째로 생성");
+        }
+        //https://minhyeokism.tistory.com/16 [programmer-dominic.kim]
+    }
+
+    
+
+
+
+
+
+
+
+    /*
 
 
     // Bubble 랜덤생성 및 삭제 함수
     void SpawnBubble1()
     {
-        if (enableSpawn)
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+        
+        if (Countdown.enableSpawn)
         {
             GameObject game1_blup_bubble1 = Instantiate(Bubble1, new Vector3(-0.3f, 1.18f, 0), Quaternion.identity);
 
@@ -45,7 +105,9 @@ public class game1_SpawnBubble : MonoBehaviour
 
     void SpawnBubble2()
     {
-        if (enableSpawn)
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+
+        if (Countdown.enableSpawn)
         {
             GameObject game1_blup_bubble2 = Instantiate(Bubble2, new Vector3(4.67f, -2.95f, 0), Quaternion.identity);
 
@@ -56,7 +118,9 @@ public class game1_SpawnBubble : MonoBehaviour
 
     void SpawnBubble3()
     {
-        if (enableSpawn)
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+
+        if (Countdown.enableSpawn)
         {
             GameObject game1_blup_bubble3 = Instantiate(Bubble3, new Vector3(-8.83f, 0.1899997f, 0), Quaternion.identity);
 
@@ -67,7 +131,9 @@ public class game1_SpawnBubble : MonoBehaviour
 
     void SpawnBubble4()
     {
-        if (enableSpawn)
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+
+        if (Countdown.enableSpawn)
         {
             GameObject game1_blup_bubble4 = Instantiate(Bubble4, new Vector2(-5.08f, -3.97f), Quaternion.identity);
 
@@ -78,7 +144,9 @@ public class game1_SpawnBubble : MonoBehaviour
 
     void SpawnBubble5()
     {
-        if (enableSpawn)
+        GameCountdown Countdown = GameObject.Find("countdown_PanelUI").GetComponent<GameCountdown>();  // GameCountdown 스크립트의 객체 받아옴
+
+        if (Countdown.enableSpawn)
         {
             GameObject game1_blup_bubble5 = Instantiate(Bubble5, new Vector2(9, -0.5800003f), Quaternion.identity);
 
@@ -86,5 +154,7 @@ public class game1_SpawnBubble : MonoBehaviour
             Destroy(game1_blup_bubble5, 2f);
         }
     }
+
+    */
 
 }
