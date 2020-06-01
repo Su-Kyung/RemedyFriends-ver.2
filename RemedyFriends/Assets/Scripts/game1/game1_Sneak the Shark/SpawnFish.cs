9b074563b0a2f48f0009136a05719a218fb07b31
Su-Kyung <slloph720@naver.com>
2020-06-02 04:30:44 +0900

    상어몰래 사냥하기 기본 틀 마련)
    - 버튼 컴포넌트 생성
    - 게임 시작하면 물고기 나타났다가 사라지기 최초 1회 실행
    
    => 이제 반복 실행 가능하게 하고, 정답 판별하고, 그에 따른 점수 추가하기하면됨!

---
+++ RemedyFriends/Assets/Scripts/game1/game1_Sneak the Shark/SpawnFish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFish : MonoBehaviour
+{
+    public GameObject Fish1, Fish2, Fish3, Fish4;   // 생성될 물고기(빨 노 초 보)
+    List<GameObject> FishList = new List<GameObject>(); // 위 물고기 리스트
+
+    void Start()
+    {
+        // 물고기 리스트에 물고기들 추가
+        FishList.Add(Fish1);
+        FishList.Add(Fish2);
+        FishList.Add(Fish3);
+        FishList.Add(Fish4);
+
+        // 물고기들 게임 시작 시 안보이게
+        HideFish();
+
+        // 게임 최초 시작
+        Invoke("ShowFish", 1);
+    }
+
+    // 랜덤으로 물고기 보여주는 함수
+    public void ShowFish()
+    {
+        int temp = Random.Range(0, FishList.Count); // temp: 리스트 중 하나 무작위로 뽑는다.
+
+        FishList[temp].SetActive(true); // 뽑힌 물고기 보여주기
+        Debug.Log(temp + 1 + "번째 물고기 맞춰야함~");
+
+        Invoke("HideFish", 0.3f);   // 금방 사라지기
+    }
+
+    // 물고기 모두 안보이게 하는 함수
+    void HideFish()
+    {
+        Fish1.SetActive(false);
+        Fish2.SetActive(false);
+        Fish3.SetActive(false);
+        Fish4.SetActive(false);
+    }
+}
