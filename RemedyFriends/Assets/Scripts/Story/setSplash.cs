6999a4016dd988814800a7e4e67e4f1a521f8389
Su-Kyung <slloph720@naver.com>
2020-06-02 21:54:19 +0900

    스플래쉬 추가
    - 소리는 X

---
+++ RemedyFriends/Assets/Scripts/Story/setSplash.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setSplash : MonoBehaviour
+{
+    public Button btnGameStart;
+    public GameObject canvasSplash;
+
+    
+    void Awake()
+    {
+        btnGameStart.gameObject.SetActive(true);
+        canvasSplash.SetActive(true);
+
+        btnGameStart.onClick.AddListener(gogoGame);
+    }
+
+    void gogoGame()
+    {
+        btnGameStart.gameObject.SetActive(false);
+        canvasSplash.SetActive(false);
+    }
+}
