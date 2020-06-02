6aa9078d5d497459edba595390a632dc9ff4a786
jjym12 <jiyoung080912@gmail.com>
2020-06-02 11:00:09 +0900

    game2 스크립트 변경
    game2 스크립트 변경

--- RemedyFriends/Assets/Scripts/game2/Get_game2_data.cs
+++ RemedyFriends/Assets/Scripts/game2/Get_game2_data.cs
@@ -14,11 +14,9 @@ public class Get_game2_data : MonoBehaviour
     string userId;
     string date;
 
-    string scoreWater;
-    string scoreCamel;
-    string scoreStuff;
-    string scoreLuna;
-    string scorePuzzle;
+    public int score;
+
+    public int count;
 
     private User_data UserData_Script;
 
@@ -30,10 +28,9 @@ public class Get_game2_data : MonoBehaviour
 
         UserData_Script = GameObject.Find("UserData").GetComponent<User_data>();
     }
-    //게임 2 우리는 목이 말라요 (시각) 
-    public string getGame2WaterScore(string currentDate)
+    //게임 2
+    public void getGame2Score(string part, string currentDate)
     {
-        date = currentDate;
         userId = UserData_Script.userId;
         Debug.LogFormat("UserID = {0}", userId);
         FirebaseDatabase.DefaultInstance.GetReference("game2").GetValueAsync().ContinueWith(task =>
@@ -51,137 +48,24 @@ public class Get_game2_data : MonoBehaviour
                     //Debug.LogFormat("Key = {0}", userIds.Key);
                     if (userIds.Key == userId)
                     {
-                        scoreWater = userIds.Child("visual").Child(date).Value.ToString();
-                        //Debug.LogFormat("scoreWater = {0}", scoreWater);
+                        ScoreList list = JsonUtility.FromJson<ScoreList>(userIds.Child(part).GetRawJsonValue());
+                        count = list.Score.Length - 1;
+                        // Debug.Log(count);
+                        //Debug.Log(list.Score[count].date);
+                        if (list.Score[count].date == currentDate)
+                        {
+                            score = list.Score[count].userscore;
+                            Debug.Log("날짜 같음");
+                            Debug.Log("userscore" + score);
+                        }
+                        else
+                        {
+                            score = 0;
+                            Debug.Log("날짜 다름");
+                        }
                     }
                 }
             }
         });
-        if (scoreWater == null || scoreWater == "")
-            return "0";
-        return scoreWater;
-    }
-    //ㄱㅔ임 2 내 낙타를 찾아줘 (청각)
-    public string getGame2CamelScore(string currentDate)
-    {
-        date = currentDate;
-        userId = UserData_Script.userId;
-        Debug.LogFormat("UserID = {0}", userId);
-        FirebaseDatabase.DefaultInstance.GetReference("game2").GetValueAsync().ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Debug.Log("error");
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-
-                foreach (var userIds in snapshot.Children)
-                {
-                    //Debug.LogFormat("Key = {0}", userIds.Key);
-                    if (userIds.Key == userId)
-                    {
-                        scoreCamel = userIds.Child("auditory").Child(date).Value.ToString();
-                        //Debug.LogFormat("scoreCamel = {0}", scoreCamel);
-                    }
-                }
-            }
-        });
-        if (scoreCamel == null || scoreCamel == "")
-            return "0";
-        return scoreCamel;
-    }
-    //게임 2 가방 속 물건을 찾아줘 (기억력)
-    public string getGame2StuffScore(string currentDate)
-    {
-        date = currentDate;
-        Debug.LogFormat("UserID = {0}", userId);
-        FirebaseDatabase.DefaultInstance.GetReference("game2").GetValueAsync().ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Debug.Log("error");
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-
-                foreach (var userIds in snapshot.Children)
-                {
-                    //Debug.LogFormat("Key = {0}", userIds.Key);
-                    if (userIds.Key == userId)
-                    {
-                        scoreStuff = userIds.Child("memory").Child(date).Value.ToString();
-                        //Debug.LogFormat("scoreStuff = {0}", scoreStuff);
-                    }
-                }
-            }
-        });
-        if (scoreStuff == null || scoreStuff == "")
-            return "0";
-        return scoreStuff;
-    }
-    //게임 2 루나와 숨바꼭질 (행동 조절) 
-    public string getGame2LunaScore(string currentDate)
-    {
-        date = currentDate;
-        userId = UserData_Script.userId;
-        Debug.LogFormat("UserID = {0}", userId);
-        FirebaseDatabase.DefaultInstance.GetReference("game2").GetValueAsync().ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Debug.Log("error");
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-
-                foreach (var userIds in snapshot.Children)
-                {
-                    //Debug.LogFormat("Key = {0}", userIds.Key);
-                    if (userIds.Key == userId)
-                    {
-                        scoreLuna = userIds.Child("control").Child(date).Value.ToString();
-                        //Debug.LogFormat("scoreLuna = {0}", scoreLuna);
-                    }
-                }
-            }
-        });
-        if (scoreLuna == null || scoreLuna == "")
-            return "0";
-        return scoreLuna;
-    }
-    //게임 2 소중한 추억을 맞춰줘 (문제해결: 범주화)
-    public string getGame2PuzzleScore(string currentDate)
-    {
-        date = currentDate;
-        userId = UserData_Script.userId;
-        Debug.LogFormat("UserID = {0}", userId);
-        FirebaseDatabase.DefaultInstance.GetReference("game2").GetValueAsync().ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Debug.Log("error");
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-
-                foreach (var userIds in snapshot.Children)
-                {
-                    //Debug.LogFormat("Key = {0}", userIds.Key);
-                    if (userIds.Key == userId)
-                    {
-                        scorePuzzle = userIds.Child("organization").Child(date).Value.ToString();
-                        //Debug.LogFormat("scorePuzzle = {0}", scorePuzzle);
-                    }
-                }
-            }
-        });
-        if (scorePuzzle == null || scorePuzzle == "")
-            return "0";
-        return scorePuzzle;
     }
 }
--- RemedyFriends/Assets/Scripts/game2/Save_game2_data.cs
+++ RemedyFriends/Assets/Scripts/game2/Save_game2_data.cs
@@ -16,6 +16,8 @@ public class Save_game2_data : MonoBehaviour
 
     private User_data UserData_Script;
 
+    List<ScoreData> objectList = new List<ScoreData>();
+
     void Awake()
     {
         firebaseApp = FirebaseDatabase.DefaultInstance.App;
@@ -24,66 +26,51 @@ public class Save_game2_data : MonoBehaviour
 
         UserData_Script = GameObject.Find("UserData").GetComponent<User_data>();
     }
-    //게임 2 우리는 목이 말라요 (시각)
-    public bool saveGame2WaterScore(int score)
+    public ScoreData createSubObject(string date, int score)
     {
-        userId = UserData_Script.userId;
-        date = System.DateTime.Now.ToString("yyyy/MM/dd");
-        if (score != null)
-        {
-            reference.Child("game2").Child(userId).Child("visual").Child(date).SetValueAsync(score);
-            Debug.Log("score 저장 성공");
-            return true;
-        }
-        return false;
+        ScoreData myscore = new ScoreData();
+        myscore.date = date;
+        myscore.userscore = score;
+        return myscore;
     }
-    //게임 2 내 낙타를 찾아줘 (청각)
-    public bool saveGame2CamelScore(int score)
+
+    public Dictionary<string, object> ToDictionary(string date, int score)
     {
-        userId = UserData_Script.userId;
-        date = System.DateTime.Now.ToString("yyyy/MM/dd");
-        if (score != null)
-        {
-            reference.Child("game2").Child(userId).Child("auditory").Child("date").Child(date).Child("score").SetValueAsync(score);
-            Debug.Log("score 저장 성공");
-            return true;
-        }
-        return false;
+        Dictionary<string, object> result = new Dictionary<string, object>();
+        result["date"] = date;
+        result["userscore"] = score;
+
+        return result;
     }
-    //게임 2 가방 속 물건을 찾아줘 (기억력)
-    public bool saveGame2StuffScore(int score)
+
+    //게임 2
+    public bool saveGame2Score(string part, int score, int preScore, int count)
     {
+        int sumscore;
+
         userId = UserData_Script.userId;
         date = System.DateTime.Now.ToString("yyyy/MM/dd");
-        if (score != null)
+
+        if (preScore == 0)
         {
-            reference.Child("game2").Child(userId).Child("memory").Child(date).SetValueAsync(score);
-            Debug.Log("score 저장 성공");
-            return true;
+            if (count == 0)
+            {
+                count = 0;
+            }
+            else { count++; }
+
+            sumscore = score;
         }
-        return false;
-    }
-    //게임 2 루나와 숨바꼭질 ( 행동조절)
-    public bool saveGame2LunaScore(int score)
-    {
-        userId = UserData_Script.userId;
-        date = System.DateTime.Now.ToString("yyyy/MM/dd");
-        if (score != null)
+        else
         {
-            reference.Child("game2").Child(userId).Child("control").Child(date).SetValueAsync(score);
-            Debug.Log("score 저장 성공");
-            return true;
+            sumscore = (int)(score + preScore) / 2;
         }
-        return false;
-    }
-    //게임2 소중한 추억을 맞춰줘 ( 문제해결 범주화)
-    public bool saveGame2PuzzleScore(int score)
-    {
-        userId = UserData_Script.userId;
-        date = System.DateTime.Now.ToString("yyyy/MM/dd");
+
         if (score != null)
         {
-            reference.Child("game2").Child(userId).Child("organization").Child(date).SetValueAsync(score);
+            //firebase에 저장
+            Dictionary<string, object> update = ToDictionary(date, sumscore);
+            reference.Child("game2").Child(userId).Child(part).Child("Score").Child(count.ToString()).UpdateChildrenAsync(update);
             Debug.Log("score 저장 성공");
             return true;
         }
